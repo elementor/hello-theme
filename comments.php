119e06fa75b4566bf21311586f4c34c4f763926b
Mati Horovitz <mati@pojo.me>
2018-03-19 13:51:18 +0200

    Hello Theme!!

---
+++ comments.php
@@ -0,0 +1,40 @@
+<section id="comments" class="comments-area">
+
+	<?php if ( have_comments() ) : ?>
+		<h3 class="title-comments">
+			<span>
+				<?php printf( _n( 'One Response', '%1$s Responses', get_comments_number(), '' ), number_format_i18n( get_comments_number() ), get_the_title() ); ?>
+			</span>
+		</h3>
+
+	<?php the_comments_navigation(); ?>
+
+	<ol class="comment-list">
+		<?php
+		wp_list_comments( array(
+			'style'       => 'ol',
+			'short_ping'  => true,
+			'avatar_size' => 42,
+		) );
+		?>
+	</ol><!-- .comment-list -->
+
+	<?php the_comments_navigation(); ?>
+
+<?php endif; // Check for have_comments(). ?>
+
+<?php
+// If comments are closed and there are comments, let's leave a little note, shall we?
+if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+	<p class="no-comments"><?php _e( 'Comments are closed.', 'elementor-theme' ); ?></p>
+<?php endif; ?>
+
+<?php
+comment_form( array(
+	'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+	'title_reply_after'  => '</h2>',
+) );
+?>
+
+</section><!-- .comments-area -->
---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+<!-- HEADER START -->
+
+<?php do_action( 'elementor/theme/header' ) ?>
+
+<!-- HEADER END -->
