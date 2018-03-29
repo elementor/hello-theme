6c6bc18fb53d8fda2f35929f309a5459e5678375
Rami Yushuvaev <r_a_m_i@hotmail.com>
2018-03-29 11:06:11 +0300

    Document functions file

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-// Set up theme support
+/**
+ * Set up theme support for various WordPress features.
+ */
 function elementor_hello_theme_setup() {
 
 	add_theme_support( 'menus' );
@@ -9,36 +11,40 @@ function elementor_hello_theme_setup() {
 	add_theme_support( 'automatic-feed-links' );
 	add_theme_support( 'title-tag' );
 	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
-	add_theme_support( 'custom-logo', array(
-		'height' => 70,
-		'width' => 350,
-		'flex-height' => true,
-		'flex-width' => true,
-	) );
+	add_theme_support( 'custom-logo', array( 'height' => 70, 'width' => 350, 'flex-height' => true, 'flex-width' => true ) );
 
 	load_theme_textdomain( 'elementor-hello-theme', get_template_directory() . '/languages' );
+
 }
 add_action( 'after_setup_theme', 'elementor_hello_theme_setup' );
 
-// Theme Scripts & Styles
+/**
+ * Enqueue scripts and styles.
+ */
 function elementor_hello_theme_scripts_styles() {
 	wp_enqueue_style( 'elementor-hello-theme-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'elementor_hello_theme_scripts_styles' );
 
-// Register Elementor Locations
+/**
+ * Register Elementor locations.
+ */
 function elementor_hello_theme_register_elementor_locations( $elementor_theme_manager ) {
 	$elementor_theme_manager->register_all_core_location();
 };
 add_action( 'elementor/theme/register_locations', 'elementor_hello_theme_register_elementor_locations' );
 
-// Remove WP Embed
+/**
+ * Remove WordPress Embed.
+ */
 function elementor_hello_theme_deregister_scripts() {
 	wp_deregister_script( 'wp-embed' );
 }
 add_action( 'wp_footer', 'elementor_hello_theme_deregister_scripts' );
 
-// Remove WP Emoji
+/**
+ * Remove WordPress Emoji.
+ */
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
