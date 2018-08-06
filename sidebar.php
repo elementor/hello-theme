c230d0f4212eab7e1072bbb36057f965d0e3c242
Ariel Klikstein <relklik@gmail.com>
2018-08-06 19:34:39 +0300

    Reformat code

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,5 @@
-<?php/** * This file is here to avoid the Deprecated Message for sidebar by wp-includes/theme-compat/sidebar.php */
+<?php
+/**
+ * This file is here to avoid the Deprecated Message for sidebar by wp-includes/theme-compat/sidebar.php
+ *
+ */
