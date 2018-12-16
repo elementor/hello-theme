67c0e0ee58c1d4b5b65bd3f5ff9de29f31a568dd
Yakir Sitbon <kingyes1@gmail.com>
2018-12-16 22:58:59 +0200

    Fix style css

--- functions.php
+++ functions.php
@@ -25,8 +25,7 @@ add_action( 'after_setup_theme', 'elementor_hello_theme_setup' );
 
 // Theme Scripts & Styles
 function elementor_hello_theme_scripts_styles() {
-	//wp_enqueue_style( 'elementor-hello-theme-style', get_stylesheet_uri() );
-	wp_enqueue_style( 'elementor-hello-theme-style', get_template_directory_uri() . '/assets/css/style.css' );
+	wp_enqueue_style( 'elementor-hello-theme-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'elementor_hello_theme_scripts_styles' );
 
