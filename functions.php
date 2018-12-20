7728b38ef7b3d2bb6db399af2e816889acf11c93
nuritsha <nurit@elementor.com>
2018-12-20 13:13:04 +0200

    misc styles & woocommerce support

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Elementor Hello Theme only works in WordPress 4.7 or later.
+ * Elementor Hello Theme requires WordPress 4.7 or later.
  */
 function elementor_hello_theme_switch_theme( $oldtheme_name, $oldtheme ) {
 	$missing_dependencies = false;
@@ -40,31 +40,42 @@ function elementor_hello_theme_switch_admin_notice() {
 /*
  * Set up theme support
  */
-if ( ! isset( $content_width ) ) {
-	$content_width = 800;
-}
-
 function elementor_hello_theme_setup() {
-	add_theme_support( 'menus' );
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'automatic-feed-links' );
-	add_theme_support( 'title-tag' );
-	add_theme_support( 'custom-logo' );
-	add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
-	add_theme_support( 'custom-logo', array(
-		'height' => 100,
-		'width' => 350,
-		'flex-height' => true,
-		'flex-width' => true,
-	) );
+	if ( apply_filters( 'elementor_hello_theme_load_textdomain', true ) ) {
+		load_theme_textdomain( 'elementor-hello-theme', get_template_directory() . '/languages' );
+	}
 
-	add_theme_support( 'woocommerce' );
+	if ( apply_filters( 'elementor_hello_theme_register_menus', true ) ) {
+		register_nav_menus( array( 'hello-menu-1' => __( 'Primary', 'elementor-hello-theme' ) ) );
+	}
 
-	register_nav_menus(
-		array( 'menu-1' => __( 'Primary', 'elementor-hello-theme' ) )
-	);
+	if ( apply_filters( 'elementor_hello_theme_add_theme_support', true ) ) {
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'custom-logo' );
+		add_theme_support( 'custom-background' );
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption'
+		) );
+		add_theme_support( 'custom-logo', array(
+			'height' => 100,
+			'width' => 350,
+			'flex-height' => true,
+			'flex-width' => true,
+		) );
 
-	load_theme_textdomain( 'elementor-hello-theme', get_template_directory() . '/languages' );
+		if ( apply_filters( 'elementor_hello_theme_add_woocommerce_support', true ) ) {
+			add_theme_support( 'woocommerce' );
+			add_theme_support( 'wc-product-gallery-zoom' );
+			add_theme_support( 'wc-product-gallery-lightbox' );
+			add_theme_support( 'wc-product-gallery-slider' );
+		}
+	}
 }
 add_action( 'after_setup_theme', 'elementor_hello_theme_setup' );
 
@@ -72,7 +83,9 @@ add_action( 'after_setup_theme', 'elementor_hello_theme_setup' );
  * Theme Scripts & Styles
  */
 function elementor_hello_theme_scripts_styles() {
-	wp_enqueue_style( 'elementor-hello-theme-style', get_stylesheet_uri() );
+	if ( apply_filters( 'elementor_hello_theme_enqueue_style', true ) ) {
+		wp_enqueue_style( 'elementor-hello-theme-style', get_stylesheet_uri() );
+	}
 }
 add_action( 'wp_enqueue_scripts', 'elementor_hello_theme_scripts_styles' );
 
@@ -80,22 +93,35 @@ add_action( 'wp_enqueue_scripts', 'elementor_hello_theme_scripts_styles' );
  * Register Elementor Locations
  */
 function elementor_hello_theme_register_elementor_locations( $elementor_theme_manager ) {
-	$elementor_theme_manager->register_all_core_location();
-};
+	if ( apply_filters( 'elementor_hello_theme_register_elementor_locations', true ) ) {
+		$elementor_theme_manager->register_all_core_location();
+	}
+}
 add_action( 'elementor/theme/register_locations', 'elementor_hello_theme_register_elementor_locations' );
 
 /*
  * Remove WP Embed
  */
 function elementor_hello_theme_deregister_scripts() {
-	wp_deregister_script( 'wp-embed' );
+	if ( apply_filters( 'elementor_hello_theme_deregister_scripts', true ) ) {
+		wp_deregister_script( 'wp-embed' );
+	}
 }
 add_action( 'wp_footer', 'elementor_hello_theme_deregister_scripts' );
 
 /*
+ * Set content width
+ */
+if ( ! isset( $content_width ) ) {
+	$content_width = 800;
+}
+
+/*
  * Remove WP Emoji
  */
-remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-remove_action( 'wp_print_styles', 'print_emoji_styles' );
-remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-remove_action( 'admin_print_styles', 'print_emoji_styles' );
+if ( apply_filters( 'elementor_hello_theme_remove_wp_actions', true ) ) {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+}
--- readme.md
+++ readme.md
@@ -7,13 +7,13 @@
 **Version:** 1.0  
 **License:** GNU General Public License v3 or later  
 **License URI:** https://www.gnu.org/licenses/gpl-3.0.html  
-**Tags:** Elementor, ElementorPro, one-column, flexible-header, accessibility-ready, custom-colors, custom-menu, custom-logo, editor-style, featured-images, footer-widgets, rtl-language-support, sticky-post, theme-options, threaded-comments, translation-ready
+**Tags:** Elementor, ElementorPro, flexible-header, accessibility-ready, custom-colors, custom-menu, custom-logo, editor-style, featured-images, rtl-language-support, threaded-comments, translation-ready
 
 The theme for Elementor.
 
 ## Description
 
-A plain-vanilla theme, best suited for building your site using Elementor & Elementor Pro.
+A plain-vanilla theme, best suited for building your site using **Elementor** & **Elementor Pro**.
 This theme resets the environment and prepare it for smooth operation of Elementor. 
 
 Please note that we do not provide support for customizing the theme (as it should not be required, for customizing your site, use **Elementor!**). 
@@ -21,20 +21,40 @@ Please note that we do not provide support for customizing the theme (as it shou
 ## Installation
 
 1. In your admin panel, go to Appearance > Themes and click the 'Add New' button.
-2. Type in 'Elementor Hello' in the search form and press the 'Enter' key on your keyboard.
+2. Type in 'Elementor Hello' in the search form and hit the 'Enter' key on your keyboard.
 3. Click on the 'Activate' button to use your new theme right away.
 4. Navigate to Elementor and start building your site.
 
-## Style
+## Customizing
+
+### Hooks
+to prevent the loading of any of the following settings, add the following code to your child-theme functions.php:
+
+`add_filter( 'choose-from-the-list-below', '__return_false' );`
+
+* `elementor_hello_theme_load_textdomain`               load theme's textdomain     
+* `elementor_hello_theme_register_menus`                register the theme's default menu location
+* `elementor_hello_theme_add_theme_support`             register the various supported features 
+* `elementor_hello_theme_add_woocommerce_support`       register woocommerce features
+* `elementor_hello_theme_enqueue_style`                 enqueue style
+* `elementor_hello_theme_register_elementor_locations`  register elementor settings 
+* `elementor_hello_theme_deregister_scripts`            remove wp-script
+* `elementor_hello_theme_remove_wp_actions`             remove WP Emoji (don't worry, Elementor will add many great icons)
+
+### Style
 
 In order to change the styling used throughout the site, use **Elementor**.
 
 However, if for some reason there is still a need to add or change the site's css, please take into account the following:
-1. files located under 'reset' directory, should **NOT** be edited directly.
-2. in order to change any of the values defined in preset/variables.scss, add your style code to custom/pre_default.scss.
-3. any new scss files should be located under custom/ directory, and imported in custom/custom.scss. 
+1. files located under `reset` directory, should **NOT** be edited directly.
+2. in order to change any of the values defined in `preset/variables.scss`, add your style code to `custom/pre_default.scss`.
+3. any new scss files should be located under `custom/` directory, and imported in `custom/custom.scss`. 
 
 ## Frequently Asked Questions
+
+* Does this theme support any plugins?
+
+Elementor Hello includes support for WooCommerce.
 
 * Can Font Style be added thru the theme's css file?
  
