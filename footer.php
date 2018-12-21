b5965ddd5e4a4868568114ab8dd6f1f2caa1f43e
DimaMinka <cdk@cdk.co.il>
2018-12-21 12:56:16 +0200

    Functions: New - Add header/footer actions for easy override from plugin/theme

--- footer.php
+++ footer.php
@@ -2,11 +2,9 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-
-if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'footer' ) ) {
-	get_template_part( 'template-parts/footer' );
-}
 ?>
+
+<?php do_action( 'elementor_hello_theme_footer' ); ?>
 
 <?php wp_footer(); ?>
 
