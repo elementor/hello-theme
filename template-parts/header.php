3aff0860e6f501ab5bfa6d99861329a3658b1222
Ariel Klikstein <relklik@gmail.com>
2019-02-12 11:26:38 +0200

    Added theme_location to the menu

--- template-parts/header.php
+++ template-parts/header.php
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php endif; ?>
 
 	<nav id="top-menu" role="navigation">
-		<?php wp_nav_menu(); ?>
+		<?php wp_nav_menu( array( 'theme_location' => 'menu-1' ) ); ?>
 	</nav>
 
 </header>
