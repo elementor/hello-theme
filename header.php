1cc35af1eb33ff8acc5c2ca7e0599816a5ef0cba
Rami Yushuvaev <r_a_m_i@hotmail.com>
2019-06-18 17:39:21 +0300

    Use HTTPS in rel profile links

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
