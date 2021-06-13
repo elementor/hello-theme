77114ddf72816328dfb9f18cb080f8cc3aa0bb70
Ron Korland <ronk@elementor.com>
2021-06-13 21:09:28 +0300

    update version to last stable + fix eslint

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -5,35 +5,35 @@ const pkg = require( '../package.json' );
 
 const version = pkg.version;
 
-const replaceInFileWithLog = async( options ) => {
+const replaceInFileWithLog = async ( options ) => {
 	const results = await replaceInFile( options );
-	console.log( 'Replacement results:', results, "options: ", options );
-}
+	console.log( 'Replacement results:', results, 'options: ', options );
+};
 
-const run = async() => {
+const run = async () => {
 	try {
 		await replaceInFileWithLog( {
 			files: './assets/scss/style.scss',
 			from: /Version:.*$/m,
-			to: `Version: ${version}`,
+			to: `Version: ${ version }`,
 		} );
 
 		await replaceInFileWithLog( {
 			files: './assets/scss/style.scss',
 			from: /Stable tag:.*$/m,
-			to: `Stable tag: ${version}`,
+			to: `Stable tag: ${ version }`,
 		} );
 
 		await replaceInFileWithLog( {
 			files: './functions.php',
 			from: /HELLO_ELEMENTOR_VERSION', '(.*?)'/m,
-			to: `HELLO_ELEMENTOR_VERSION', '${version}'`,
+			to: `HELLO_ELEMENTOR_VERSION', '${ version }'`,
 		} );
-
-	} catch (err) {
+	} catch ( err ) {
+		// eslint-disable-next-line no-console
 		console.error( 'Error occurred:', err );
 		process.exit( 1 );
 	}
-}
+};
 
 run();
