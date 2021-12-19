f9f6f34fcb85c63fb4a77b8b933cacf2c1b3ee68
Ariel Klikstein <ariel@elementor.com>
2021-12-19 16:43:23 +0200

    Fix duplicate

--- header.php
+++ header.php
@@ -18,7 +18,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
 	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="profile" href="<?php echo set_url_scheme('http://gmpg.org/xfn/11'); ?>">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
