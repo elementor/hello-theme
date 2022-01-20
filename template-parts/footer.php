f84e4a4dd9ba5cd42465f9c24ed1d562e72bc429
Hein van Vlastuin <vv@abanganimedia.co.za>
2022-01-20 15:32:16 +0200

    added footer menu - without elementor

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -8,7 +8,17 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$footer_nav_menu = wp_nav_menu( [
+	'theme_location' => 'menu-2',
+	'fallback_cb' => false,
+	'echo' => false,
+] );
 ?>
 <footer id="site-footer" class="site-footer" role="contentinfo">
-	<?php // footer. ?>
+	<?php if ( $footer_nav_menu ) : ?>
+		<nav class="site-navigation" role="navigation">
+			<?php echo $footer_nav_menu; ?>
+		</nav>
+	<?php endif; ?>
 </footer>
