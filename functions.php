b2a1d3108121ea8425adde54b229abe5b4c606bb
jeantardelli <jeantardelli@gmail.com>
2022-05-15 01:52:57 +0200

    Fix Nav Function

--- functions.php
+++ functions.php
@@ -239,7 +239,7 @@ add_action('wp_head', 'wpb_hook_javascript');
 // Don't load Gutenberg-related stylesheets.
 add_action( 'wp_enqueue_scripts', 'remove_block_css', 100 );
 function remove_block_css() {
-    wp_dequeue_style( 'wp-block-library-theme' ); // Wordpress core
+    wp_dequeue_style( 'wp-block-library-theme' ); // Wordpress core
     wp_dequeue_style( 'wc-block-style' ); // WooCommerce
     wp_dequeue_style( 'storefront-gutenberg-blocks' ); // Storefront theme
 }
@@ -428,3 +428,70 @@ function total_comments_number () {
 	}
 	echo ( "Comentários: " . $comments );
 }
+
+
+
+// Numbere Pagination
+function numeric_posts_nav() {
+    if( is_singular() )
+        return;
+    global $wp_query; 
+    /** Stop execution if there's only 1 page */
+    if( $wp_query->max_num_pages <= 1 )
+        return;
+    $paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+    $max   = intval( $wp_query->max_num_pages );
+    /** Add current page to the array */
+    if ( $paged >= 1 )
+        $links[] = $paged;
+    /** Add the pages around the current page to the array */
+    if ( $paged >= 3 ) {
+        $links[] = $paged - 1;
+        $links[] = $paged - 2;
+    }
+    if ( ( $paged + 2 ) <= $max ) {
+        $links[] = $paged + 2;
+        $links[] = $paged + 1;
+    } 
+    echo '<div class="navigation"><ul>' . "\n"; 
+    /** Previous Post Link */
+    if ( get_previous_posts_link() )
+        printf( '<li>%s</li>' . "\n", get_previous_posts_link() );
+    /** Link to first page, plus ellipses if necessary */
+    if ( ! in_array( 1, $links ) ) {
+        $class = 1 == $paged ? ' class="active"' : ''; 
+        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' ); 
+        if ( ! in_array( 2, $links ) )
+            echo '<li>…</li>';
+    } 
+    /** Link to current page, plus 2 pages in either direction if necessary */
+    sort( $links );
+    foreach ( (array) $links as $link ) {
+        $class = $paged == $link ? ' class="active"' : '';
+        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
+    } 
+    /** Link to last page, plus ellipses if necessary */
+    if ( ! in_array( $max, $links ) ) {
+        if ( ! in_array( $max - 1, $links ) )
+            echo '<li>…</li>' . "\n"; 
+        $class = $paged == $max ? ' class="active"' : '';
+        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
+    } 
+    /** Next Post Link */
+    if ( get_next_posts_link() )
+        printf( '<li>%s</li>' . "\n", get_next_posts_link() );
+    echo '</ul></div>' . "\n"; 
+}
+
+/* AMP */
+add_action( 'amp_post_template_css', function() {
+      ?>
+      /* Put Custom CSS below */
+@media (max-width: 767px) {
+#comments .comment .avatar {
+    position: relative;
+    float: left;
+    }
+  }
+      <?php
+} );
