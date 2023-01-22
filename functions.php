cd1d60ae710b2eaebf0beabf46f8f368112867f9
Rami Yushuvaev <ramiy@elementor.com>
2023-01-22 16:59:37 +0200

    Tweak: Remove backwards compatibility support for `wp_body_open()`

--- functions.php
+++ functions.php
@@ -205,16 +205,3 @@ if ( ! function_exists( 'hello_elementor_check_hide_title' ) ) {
 	}
 }
 add_filter( 'hello_elementor_page_title', 'hello_elementor_check_hide_title' );
-
-/**
- * Wrapper function to deal with backwards compatibility.
- */
-if ( ! function_exists( 'hello_elementor_body_open' ) ) {
-	function hello_elementor_body_open() {
-		if ( function_exists( 'wp_body_open' ) ) {
-			wp_body_open();
-		} else {
-			do_action( 'wp_body_open' );
-		}
-	}
-}
