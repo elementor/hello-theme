27e31442b1aa69abff566c92786c74eeab3a06fd
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2023-03-20 23:39:12 +0200

    Tweak: Add filter to enable/disable skip link and a filter to change skip link URL [ED-10179] (#269)
    * Tweak: Add filter to enable/disable skip link and a filter to change skip link URL
    
    * Move viewport_content to the top

--- header.php
+++ header.php
@@ -10,12 +10,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+$viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' );
+$enable_skip_link = apply_filters( 'hello_elementor_enable_skip_link', true );
+$skip_link_url = apply_filters( 'hello_elementor_skip_link_url', '#content' );
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<?php $viewport_content = apply_filters( 'hello_elementor_viewport_content', 'width=device-width, initial-scale=1' ); ?>
 	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
@@ -24,8 +27,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php wp_body_open(); ?>
 
-<a class="skip-link screen-reader-text" href="#content">
-	<?php echo esc_html__( 'Skip to content', 'hello-elementor' ); ?></a>
+<?php if ( $enable_skip_link ) { ?>
+<a class="skip-link screen-reader-text" href="<?php esc_url( $skip_link_url ); ?>"><?php echo esc_html__( 'Skip to content', 'hello-elementor' ); ?></a>
+<?php } ?>
 
 <?php
 if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'header' ) ) {
