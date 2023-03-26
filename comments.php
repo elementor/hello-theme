e8dc535e25c7dd32fab061445fb6ccdd7c159a30
Yakir Sitbon <kingyes1@gmail.com>
2023-03-26 17:25:12 +0300

    Tweak: When comments is closed to post, display it to the user

--- comments.php
+++ comments.php
@@ -62,7 +62,7 @@ if ( comments_open() && get_option( 'thread_comments' ) ) {
 
 <?php
 // If comments are closed and there are comments, let's leave a little note, shall we?
-if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+if ( ! comments_open() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
 	<p class="no-comments"><?php echo esc_html__( 'Comments are closed.', 'hello-elementor' ); ?></p>
 <?php endif; ?>
