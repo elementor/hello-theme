d59ff290a58070a3c317c28418c1920ecf9e1527
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2023-07-20 06:29:45 -0700

    Tweak: Added customizer control to enable/disable description meta tag [ED-11454] (#309)
    * Tweak: Added control to enable/disable description meta tag
    
    * PHPCS
    
    * PHPCS
    
    * Merge to one section

--- includes/customizer-functions.php
+++ includes/customizer-functions.php
@@ -5,21 +5,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Register Customizer controls which add Elementor deeplinks
+ * Register Customizer controls for Hello Elementor theme.
  *
  * @return void
  */
 add_action( 'customize_register', 'hello_customizer_register' );
 function hello_customizer_register( $wp_customize ) {
+
+	// Theme settings section
+
 	require get_template_directory() . '/includes/customizer/elementor-upsell.php';
 
 	$wp_customize->add_section(
 		'hello_theme_options',
 		[
-			'title' => esc_html__( 'Header &amp; Footer', 'hello-elementor' ),
+			'title' => esc_html__( 'Theme Settings', 'hello-elementor' ),
+			'description' => esc_html__( 'Customize your Hello Elementor theme settings.', 'hello-elementor' ),
+			'description_hidden' => false,
 			'capability' => 'edit_theme_options',
 		]
 	);
+
+	// Description meta tag
+
+	$wp_customize->add_setting(
+		'hello_elementor_description_meta_tag',
+		[
+			'sanitize_callback' => false,
+			'transport' => 'refresh',
+		]
+	);
+
+	$wp_customize->add_control(
+		'hello_elementor_description_meta_tag_control',
+		[
+			'label' => esc_html__( 'Enable description meta tag', 'hello-elementor' ),
+			'default' => true,
+			'type' => 'checkbox',
+			'section' => 'hello_theme_options',
+			'settings' => 'hello_elementor_description_meta_tag',
+		]
+	);
+
+	// Header & Footer promotion
 
 	$wp_customize->add_setting(
 		'hello-elementor-header-footer',
