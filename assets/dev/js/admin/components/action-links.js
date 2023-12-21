a22696cf2babe7e2a46006cf2eefe66be469eec7
Rami Yushuvaev <ramiy@elementor.com>
2023-12-21 11:25:43 +0200

    Update

--- assets/dev/js/admin/components/action-links.js
+++ assets/dev/js/admin/components/action-links.js
@@ -1,9 +1,9 @@
 export const ActionLinks = ( { image, title, description, buttonText, link } ) => {
 	return (
-		<div className="hello_elementor__action_link">
+		<div className="hello_elementor__action_links">
 			<img src={ image } alt="Promotion" />
-			<p className="action_link_title">{ title }</p>
-			<p className="action_link_description">{ description }</p>
+			<p className="hello_elementor__action_links__title">{ title }</p>
+			<p className="hello_elementor__action_links__description">{ description }</p>
 			<a className="components-button is-secondary" href={ link } target="_blank" rel="noreferrer">{ buttonText }</a>
 		</div>
 	);
