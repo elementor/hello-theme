60c740b90cf35fcdc66f087fe3d5900649f2df4e
Yakir Sitbon <kingyes1@gmail.com>
2024-01-24 14:49:03 +0200

    Update changelog

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_VERSION', '3.0.0' );
+define( 'HELLO_ELEMENTOR_VERSION', '3.0.1' );
 
 if ( ! isset( $content_width ) ) {
 	$content_width = 800; // Pixels.
