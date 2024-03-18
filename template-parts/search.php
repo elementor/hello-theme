18e1a952bedaf53cef1d42cc8da5d7b6040bf419
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2024-03-18 14:48:33 -0700

    Fix: Wrap  page`<h1>` with `<div>`, not `<header>` (#374)

--- template-parts/search.php
+++ template-parts/search.php
@@ -12,12 +12,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 <main id="content" class="site-main">
 
 	<?php if ( apply_filters( 'hello_elementor_page_title', true ) ) : ?>
-		<header class="page-header">
+		<div class="page-header">
 			<h1 class="entry-title">
 				<?php echo esc_html__( 'Search results for: ', 'hello-elementor' ); ?>
 				<span><?php echo get_search_query(); ?></span>
 			</h1>
-		</header>
+		</div>
 	<?php endif; ?>
 
 	<div class="page-content">
--- template-parts/single.php
+++ template-parts/single.php
@@ -16,9 +16,9 @@ while ( have_posts() ) :
 <main id="content" <?php post_class( 'site-main' ); ?>>
 
 	<?php if ( apply_filters( 'hello_elementor_page_title', true ) ) : ?>
-		<header class="page-header">
+		<div class="page-header">
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header>
+		</div>
 	<?php endif; ?>
 
 	<div class="page-content">
