c54f84a189b7227bfab73e03ec264634d3fe4f1c
Rami Yushuvaev <92088692+rami-elementor@users.noreply.github.com>
2024-03-19 02:00:32 -0700

    Fix: Display `the_tags()` after `wp_link_pages()` (#379)

--- template-parts/single.php
+++ template-parts/single.php
@@ -24,13 +24,13 @@ while ( have_posts() ) :
 	<div class="page-content">
 		<?php the_content(); ?>
 
+		<?php wp_link_pages(); ?>
+
 		<?php if ( has_tag() ) : ?>
 		<div class="post-tags">
 			<?php the_tags( '<span class="tag-links">' . esc_html__( 'Tagged ', 'hello-elementor' ), ', ', '</span>' ); ?>
 		</div>
 		<?php endif; ?>
-
-		<?php wp_link_pages(); ?>
 	</div>
 
 	<?php comments_template(); ?>
