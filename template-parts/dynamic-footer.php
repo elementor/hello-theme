0dcbe12dc8332f4a77bbe84bd7b912fc780f25f9
Rami Yushuvaev <ramiy@elementor.com>
2024-05-01 09:38:25 +0300

    Update

--- template-parts/dynamic-footer.php
+++ template-parts/dynamic-footer.php
@@ -17,7 +17,7 @@ $footer_nav_menu = wp_nav_menu( [
 	'theme_location' => 'menu-2',
 	'fallback_cb' => false,
 	'echo' => false,
-	'container' => '',
+	'container' => false,
 ] );
 ?>
 <footer id="site-footer" class="site-footer dynamic-footer <?php echo esc_attr( $footer_class ); ?>">
--- template-parts/footer.php
+++ template-parts/footer.php
@@ -13,7 +13,7 @@ $footer_nav_menu = wp_nav_menu( [
 	'theme_location' => 'menu-2',
 	'fallback_cb' => false,
 	'echo' => false,
-	'container' => '',
+	'container' => false,
 ] );
 ?>
 <footer id="site-footer" class="site-footer">
