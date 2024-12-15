9151fec8ed66474dd0ea9247a0804b8857b4c3f3
Yakir Sitbon <kingyes1@gmail.com>
2024-12-15 14:29:25 +0200

    Update readme

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_VERSION', '3.1.1' );
+define( 'HELLO_ELEMENTOR_VERSION', '3.2.0' );
 
 if ( ! isset( $content_width ) ) {
 	$content_width = 800; // Pixels.
