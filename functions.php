69b080f3a8da4ef2f93c2217111133d0466aa403
Rami Yushuvaev <ramiy@elementor.com>
2024-12-16 13:25:15 +0200

    Update

--- functions.php
+++ functions.php
@@ -60,16 +60,16 @@ if ( ! function_exists( 'hello_elementor_setup' ) ) {
 					'flex-width'  => true,
 				]
 			);
-
-			/*
-			 * Editor Style.
-			 */
-			add_editor_style( 'classic-editor.css' );
-
-			/*
-			 * Gutenberg wide images.
-			 */
 			add_theme_support( 'align-wide' );
+			add_theme_support( 'responsive-embeds' );
+			remove_theme_support( 'block-templates' );
+			remove_theme_support( 'block-template-parts' );
+
+			/*
+			 * Editor Styles
+			 */
+			add_theme_support( 'editor-styles' );
+			add_editor_style( 'editor-styles.css' );
 
 			/*
 			 * WooCommerce.
