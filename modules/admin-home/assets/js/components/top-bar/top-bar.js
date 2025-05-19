c79dadce64db3e9e9ca7547ba254cf3bccf66b27
Giorgos Sarigiannidis <gsarig@users.noreply.github.com>
2025-05-19 21:15:59 +0300

    Fix: RTL alignment of the top bar [TMZ-612](#482)

--- modules/admin-home/assets/js/components/top-bar/top-bar.js
+++ modules/admin-home/assets/js/components/top-bar/top-bar.js
@@ -3,7 +3,7 @@ import { TopBarContent } from './top-bar-content';
 
 export const TopBar = () => {
 	return (
-		<AppBar position="absolute" sx={ { width: 'calc(100% - 160px)', top: 0, right: 0, height: 50, minHeight: 50, backgroundColor: 'background.default' } }>
+		<AppBar position="absolute" sx={ { width: 'calc(100% - 160px)', top: 0, right: 'unset', insetInlineEnd: 0, height: 50, minHeight: 50, backgroundColor: 'background.default' } }>
 			<TopBarContent />
 		</AppBar>
 	);
