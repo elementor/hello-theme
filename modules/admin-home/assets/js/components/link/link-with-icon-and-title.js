1539c5361162a6626afc4327432a814b2feda1d1
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-06-04 13:59:27 +0200

    refactor react components

--- modules/admin-home/assets/js/components/link/link-with-icon-and-title.js
+++ modules/admin-home/assets/js/components/link/link-with-icon-and-title.js
@@ -1,56 +1,26 @@
 import Stack from '@elementor/ui/Stack';
-import Typography from '@elementor/ui/Typography';
-import Link from '@elementor/ui/Link';
-import Box from '@elementor/ui/Box';
-import { decode } from 'html-entities';
-import DynamicIcon from '../dynamic-icon';
+import SublinksList from './sub-links-list';
+import LinkTitleWithIcon from './link-title-with-icon';
 
 export const LinkWithIconAndTitle = ( {
-		title,
-		link = null,
-		icon = 'SettingsIcon',
-		sublinks = [],
-		onClick = () => {},
-        target = '',
-	} ) => {
+	title,
+	link = null,
+	icon = 'SettingsIcon',
+	sublinks = [],
+	onClick = () => {},
+	target = '',
+} ) => {
 	return (
-		<Stack direction="row" gap={ 1 } sx={ { alignContent: 'flex-start' } }>
-			<DynamicIcon componentName={ icon } fontSize="tiny" color="text.primary" sx={ { pt: 0.2 } } />
-			<Stack direction="column">
-				<Typography variant="subtitle1" color="text.primary">
-					{
-						link && ( 0 === sublinks.length ) ? (
-							<Link color="inherit" underline="hover" onClick={ onClick } href={ link } target={ target } sx={ { lineHeight: 'initial', fontWeight: 'normal' } }>
-								{ decode( title ) }
-							</Link>
-						) : (
-							<span style={ { lineHeight: 'initial', fontWeight: 'normal' } }>
-								{ decode( title ) }
-							</span>
-						)
-					}
-				</Typography>
-				{ sublinks.length > 0 && (
-				<Box sx={ { mt: 0.5 } }>
-					<Typography variant="body2" color="text.secondary">
-						{ sublinks.map( ( sublink, index ) => (
-							<Box component="span" key={ index }>
-								{ index > 0 && <span style={ { margin: '0 6px' } }>|</span> }
-								<Link
-									color="inherit"
-									underline="hover"
-									href={ sublink.link }
-									target={ sublink.target || target }
-									sx={ { lineHeight: 'initial', fontWeight: 'normal' } }
-                                    >
-									{ decode( sublink.title ) }
-								</Link>
-							</Box>
-                            ) ) }
-					</Typography>
-				</Box>
-                ) }
-			</Stack>
+		<Stack direction="column">
+			<LinkTitleWithIcon
+				title={title}
+				link={link}
+				icon={icon}
+				sublinks={sublinks}
+				onClick={onClick}
+				target={target}
+			/>
+			{ sublinks.length > 0 && <SublinksList sublinks={sublinks} target={target} /> }
 		</Stack>
 	);
 };
