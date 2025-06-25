6c0291fb2e8a34a5a30f17160da951e8e395a25c
Giorgos Sarigiannidis <gsarig@users.noreply.github.com>
2025-06-25 11:59:31 +0300

    Fixes the path to the `editor-styles.css` [TMZ-707] (#499)

--- functions.php
+++ functions.php
@@ -80,7 +80,7 @@ if ( ! function_exists( 'hello_elementor_setup' ) ) {
 			 * Editor Styles
 			 */
 			add_theme_support( 'editor-styles' );
-			add_editor_style( 'editor-styles.css' );
+			add_editor_style( 'assets/css/editor-styles.css' );
 
 			/*
 			 * WooCommerce.
