483e74358548a6893d8bc922864b47535ac9057c
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-09-08 17:23:22 +0200

    Tweak: improve changelog appearance [TMZ-928] (#543)

--- modules/admin-home/assets/js/components/settings/update.js
+++ modules/admin-home/assets/js/components/settings/update.js
@@ -1,11 +1,19 @@
-import Stack from '@elementor/ui/Stack';
+import Box from '@elementor/ui/Box';
 import Typography from '@elementor/ui/Typography';
+import { useMemo } from 'react';
 
 export default function Update( { title, description } ) {
+	const descriptionToShow = useMemo( () => {
+		const parser = new DOMParser();
+		const doc = parser.parseFromString( description, 'text/html' );
+		const listItems = doc.querySelectorAll( 'li' );
+		const extractedContent = Array.from( listItems ).map( ( item ) => item.textContent.trim() );
+		return extractedContent.join( '\n' );
+	}, [ description ] );
     return (
-	<Stack direction={ 'column' } gap={ 2 }>
-		<Typography variant={ 'h6' }>{ title }</Typography>
-		<div dangerouslySetInnerHTML={ { __html: description } } />
-	</Stack>
+	<Box sx={ { py: 2, px: 3 } }>
+		<Typography variant={ 'subtitle1' } sx={ { pb: 0.75 } }>{ title }</Typography>
+		<Typography variant={ 'body2' } sx={ { whiteSpace: 'pre-line' } }>{ descriptionToShow }</Typography>
+	</Box>
     );
 }
