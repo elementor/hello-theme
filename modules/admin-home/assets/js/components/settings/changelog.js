4b07043127c314ff23fd6b68f304510525a6b205
Nicola Peluchetti <nicola.peluchetti@gmail.com>
2025-09-08 16:16:13 +0200

    refactor gitignore

---
+++ modules/admin-home/assets/js/components/settings/changelog.js
@@ -0,0 +1,43 @@
+import Dialog from '@elementor/ui/Dialog';
+import DialogContent from '@elementor/ui/DialogContent';
+import DialogHeader from '@elementor/ui/DialogHeader';
+import Typography from '@elementor/ui/Typography';
+import { __ } from '@wordpress/i18n';
+import Stack from '@elementor/ui/Stack';
+import Update from './update';
+import Divider from '@elementor/ui/Divider';
+import { PlusIcon } from './plus-icon';
+
+export const ChangelogDialog = ( { open, onClose, whatsNew } ) => {
+	return (
+		<Dialog
+			open={ open }
+			onClose={ onClose }
+			maxWidth="sm"
+			fullWidth
+		>
+			<DialogHeader
+				onClose={ onClose }
+				variant="outlined"
+				logo={ <PlusIcon /> }
+			>
+				<Typography variant="overline" sx={ { textTransform: 'uppercase', fontWeight: 400, color: 'text.primary' } }>
+					{ __( 'Changelog', 'hello-elementor' ) }
+				</Typography>
+			</DialogHeader>
+
+			<DialogContent sx={ { p: 0 } }>
+				<Stack direction={ 'column' } >
+					{ whatsNew.map( ( item, index ) => {
+							return (
+								<>
+									<Update key={ item.id } { ...item } />
+									{ index !== whatsNew.length - 1 && <Divider /> }
+								</>
+							);
+						} ) }
+				</Stack>
+			</DialogContent>
+		</Dialog>
+	);
+};
